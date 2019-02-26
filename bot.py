210d376f381599d496af4ea630fb0dd911ec07a8
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-02-26 10:32:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,22 +3,19 @@ import sys
 import tweepy
 from datetime import date
 
-#from generate_advertisement import update_Craiglist
-
-# from credentials import *  # use this one for testing
 
 # use this for production; set vars in heroku dashboard
 from os import environ
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+Consumer_key= environ['Consumer_key']
+CONSUMER_SECRET = environ['Consumer_secret']
+Access_key = environ['Access_key']
+Access_secret = environ['Access_secret']
 
 #INTERVAL = 60 * 60 * 6  # tweet every 6 hours
  INTERVAL = 15  # every 15 seconds, for testing
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
+auth.set_access_token(Access_key, Access_secret)
 api = tweepy.API(auth)
 
 while True:
