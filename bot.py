fb0d54ab7cec1f88329d10b933b16b66b4626b27
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-02-26 10:51:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,15 @@
-import time
-import sys
+#import sys
 import tweepy
 from datetime import date
 
 
 from os import environ
 Consumer_key= environ['Consumer_key']
-CONSUMER_SECRET = environ['Consumer_secret']
+Consumer_secret = environ['Consumer_secret']
 Access_key = environ['Access_key']
 Access_secret = environ['Access_secret']
 
-#INTERVAL = 60 * 60 * 6  # tweet every 6 hours
-INTERVAL = 15  # every 15 seconds, for testing
+
 
 auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
 auth.set_access_token(Access_key, Access_secret)
