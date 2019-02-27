04e184f7e88e0fe9f187725f00d3e59315bb94c7
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-02-27 17:26:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@
 import tweepy
 from datetime import date
 from messages import get_message
+import gspread
+import json
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 from os import environ
@@ -10,11 +13,18 @@ Consumer_secret = environ['Consumer_secret']
 Access_key = environ['Access_key']
 Access_secret = environ['Access_secret']
 
+json_cred = envirion['google_cred']
 
 
 auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
 auth.set_access_token(Access_key, Access_secret)
 api = tweepy.API(auth)
+
+scopes = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict=json_cred, scopes=scopes)
+gc = gspread.authorize(creds)
+
 
 if __name__=='__main__':
     
